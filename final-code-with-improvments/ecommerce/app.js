dbe4844a592ad137b6f00cad81452e146ecbe9df
Richard <grfld93@yahoo.com>
2020-04-22 22:23:55 -0700

    fixing cors

--- final-code-with-improvments/ecommerce/app.js
+++ final-code-with-improvments/ecommerce/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const cors = require("cors");
 const expressValidator = require('express-validator');
 require('dotenv').config();
 // import routes
